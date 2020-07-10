54fbed2d20ff8daec5cda02b03802de9a138129a
pandongwei <usrki@student.kit.edu>
2020-07-10 13:06:48 +0200

    commit own change for training

--- eval/dataset.py
+++ eval/dataset.py
@@ -98,3 +98,44 @@ class cityscapes(Dataset):
     def __len__(self):
         return len(self.filenames)
 
+class freiburgForest(Dataset):
+
+    def __init__(self, root, input_transform=None, target_transform=None, subset='test'):
+        self.images_root = os.path.join(root, subset + '/rgb')
+        self.labels_root = os.path.join(root, subset + '/GT_color')
+        # print (self.images_root)
+        # self.filenames = [image_basename(f) for f in os.listdir(self.images_root) if is_image(f)]
+        self.filenames = [os.path.join(self.images_root, f) for f in os.listdir(self.images_root)]
+        self.filenames.sort()
+        self.filenamesGt = [os.path.join(self.labels_root, f) for f in os.listdir(self.labels_root)]
+        self.filenamesGt.sort()
+
+        self.input_transform = input_transform  # ADDED THIS
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        filename = self.filenames[index]
+        filenameGt = self.filenamesGt[index]
+        # print("111111111111111111111111111 ", filename)
+        # print("f2222222222222222222222222222222222  ",filenameGt)
+        with open(image_path_city(self.images_root, filename), 'rb') as f:
+            image = load_image(f).convert('RGB')
+        with open(image_path_city(self.labels_root, filenameGt), 'rb') as f:
+            label = load_image(f).convert('L')
+        # print("image:   ",image.getpixel((0,0)))
+        # print("label:   ",label.getpixel((0,0)))
+        if self.input_transform is not None:
+            image = self.input_transform(image)
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+        # print("image:   ",image.size())
+        # print("label:   ",label.size())
+        # temp = set()
+        # for i in range(512):
+        #     for j in range(1024):
+        #         temp.add(int(label[0][i][j].numpy()))
+        # print("111111111111111111111",temp)
+        return image, label, filename, filenameGt
+
+    def __len__(self):
+        return len(self.filenames)
---
+++ eval/eval_freiburgforest.py
@@ -0,0 +1,135 @@
+# Code to produce colored segmentation output in Pytorch for all cityscapes subsets  
+# Sept 2017
+# Eduardo Romera
+#######################
+
+import numpy as np
+import torch
+import os
+import importlib
+
+from PIL import Image
+from argparse import ArgumentParser
+
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, CenterCrop, Normalize, Resize
+from torchvision.transforms import ToTensor, ToPILImage
+
+from dataset import freiburgForest
+from erfnet import ERFNet
+from transform import Relabel, ToLabel, Colorize
+
+import visdom
+
+
+NUM_CHANNELS = 3
+NUM_CLASSES = 7
+
+image_transform = ToPILImage()
+input_transform_cityscapes = Compose([
+    Resize((512,1024),Image.BILINEAR),
+    ToTensor(),
+    #Normalize([.485, .456, .406], [.229, .224, .225]),
+])
+target_transform_cityscapes = Compose([
+    Resize((512,1024),Image.NEAREST),
+    ToLabel(),
+    Relabel(0, 1),
+    Relabel(35, 2),
+    Relabel(96, 2),
+    Relabel(100, 4),
+    Relabel(150, 5),
+    Relabel(170, 6),
+    Relabel(255, 3),
+])
+
+def main(args):
+
+    modelpath = args.loadDir + args.loadModel
+    #weightspath = args.loadDir + args.loadWeights #TODO
+    weightspath = "../save/feriburgForest_1/model_best.pth"
+    print ("Loading model: " + modelpath)
+    print ("Loading weights: " + weightspath)
+
+    #Import ERFNet model from the folder
+    #Net = importlib.import_module(modelpath.replace("/", "."), "ERFNet")
+    model = ERFNet(NUM_CLASSES)
+  
+    model = torch.nn.DataParallel(model)
+    if (not args.cpu):
+        model = model.cuda()
+
+    #model.load_state_dict(torch.load(args.state))
+    #model.load_state_dict(torch.load(weightspath)) #not working if missing key
+
+    def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
+        own_state = model.state_dict()
+        for name, param in state_dict.items():
+            if name not in own_state:
+                 continue
+            own_state[name].copy_(param)
+        return model
+
+    model = load_my_state_dict(model, torch.load(weightspath))
+    print("Model and weights LOADED successfully")
+
+    model.eval()
+
+    if(not os.path.exists(args.datadir)):
+        print ("Error: datadir could not be loaded")
+
+
+    loader = DataLoader(freiburgForest(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset = args.subset),
+        num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
+
+    # For visualizer:
+    # must launch in other window "python3.6 -m visdom.server -port 8097"
+    # and access localhost:8097 to see it
+    if (args.visualize):
+        vis = visdom.Visdom()
+
+    for step, (images, labels, filename, filenameGt) in enumerate(loader):
+        if (not args.cpu):
+            images = images.cuda()
+            #labels = labels.cuda()
+
+        inputs = Variable(images)
+        #targets = Variable(labels)
+        with torch.no_grad():
+            outputs = model(inputs)
+
+        label = outputs[0].max(0)[1].byte().cpu().data
+        #label_cityscapes = cityscapes_trainIds2labelIds(label.unsqueeze(0))
+        label_color = Colorize()(label.unsqueeze(0))
+
+        filenameSave = "./freiburgforest_1/" + filename[0].split("freiburg_forest_annotated/")[0]
+        os.makedirs(os.path.dirname(filenameSave), exist_ok=True)
+        #image_transform(label.byte()).save(filenameSave)      
+        label_save = ToPILImage()(label_color)           
+        label_save.save(filenameSave) #TODO
+
+        if (args.visualize):
+            vis.image(label_color.numpy())
+        print (step, filenameSave)
+
+    
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"  ## todo
+    parser = ArgumentParser()
+
+    parser.add_argument('--state')
+
+    parser.add_argument('--loadDir',default="../trained_models/")
+    parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
+    parser.add_argument('--loadModel', default="erfnet.py")
+    parser.add_argument('--subset', default="test")  #can be val, test, train, demoSequence
+
+    parser.add_argument('--datadir', default="/mrtstorage/users/pan/freiburg_forest_multispectral_annotated/freiburg_forest_annotated/")
+    parser.add_argument('--num-workers', type=int, default=4)
+    parser.add_argument('--batch-size', type=int, default=1)
+    parser.add_argument('--cpu', action='store_true')
+
+    parser.add_argument('--visualize', action='store_true')
+    main(parser.parse_args())
